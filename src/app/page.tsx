e0ad4487cacecd48abbd8389240d98e19272d09a
Gerardo <gerardo.mayboca@nimblegravity.com>
2024-07-16 18:58:35 -0700

    Added more styles to checkout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,164 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 
 import {
   AspectRatio,
-   Box,
-   Card,
-   CardContent,
-   FormControl,
-   Grid,
-   IconButton,
-   Input,
-   Stack,
-   Tab,
-   TabList,
-   Tabs,
-   Typography,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  Sheet,
+  Stack,
+  Tab,
+  TabList,
+  Tabs,
+  Textarea,
+  Typography,
 } from "@mui/joy";
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
-import { Header, Sidebar } from "@/components";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import SearchIcon from "@mui/icons-material/Search";
+import { DashboardLayout } from "@/components";
 import coffeImg from "@/assets/images/coffe.jpg";
 
 export default function Home() {
-   return (
-      <React.Fragment>
-         <Box sx={{ display: "flex", minHeight: "100dvh" }}>
-            <Header />
-            <Sidebar />
-            <Stack
-               component="main"
-               className="MainContent"
-               spacing={2}
-               sx={{
-                  px: { xs: 2, md: 6 },
-                  pt: {
-                     xs: "calc(12px + var(--Header-height))",
-                     sm: "calc(12px + var(--Header-height))",
-                     md: 3,
-                  },
-                  pb: { xs: 2, sm: 2, md: 3 },
-                  flex: 1,
-                  display: "flex",
-                  flexDirection: "column",
-                  minWidth: 0,
-                  height: "100dvh",
-                  gap: 1,
-               }}
-            >
-               <Typography level="h1">Orden</Typography>
-               <FormControl>
-                  <Input startDecorator={<SearchIcon />} placeholder="Buscar comida, bebidas, etc..." />
-               </FormControl>
-               <Tabs size="md">
-                  <TabList>
-                     <Tab>Bebidas</Tab>
-                     <Tab>Panaderia</Tab>
-                     <Tab>Otros</Tab>
-                  </TabList>
-               </Tabs>
-               <Grid container spacing={2}>
-                  {Array.from({ length: 10 }).map((_, index) => (
-                     <Grid xs={6} md={4} xl={2}>
-                        <Card>
-                           <div>
-                              <Typography level="title-lg">
-                                 Americano
-                              </Typography>
-                              <AspectRatio minHeight="120px" maxHeight="200px">
-                                <Image src={coffeImg} alt="product-image" />
-                              </AspectRatio>
-                           </div>
-                           <CardContent orientation="horizontal" sx={{ justifyContent: "space-between"}}>
-                              <div>
-                                 <Typography level="body-xs">
-                                    Precio total:
-                                 </Typography>
-                                 <Typography fontSize="lg" fontWeight="lg">
-                                    $45.00
-                                 </Typography>
-                              </div>
-                              <IconButton size="lg" variant="soft">
-                                <AddIcon />
-                              </IconButton>
-                           </CardContent>
-                        </Card>
-                     </Grid>
-                  ))}
-               </Grid>
+  return (
+    <DashboardLayout>
+      <Stack direction="row" flex={1} height="100%">
+        <Stack spacing={2} p={4} height="100%">
+          <Typography level="h1">Orden</Typography>
+
+          <FormControl>
+            <Input
+              startDecorator={<SearchIcon />}
+              placeholder="Buscar comida, bebidas, etc..."
+            />
+          </FormControl>
+
+          <Tabs size="md" variant="plain" sx={{ bgcolor: "inherit" }}>
+            <TabList>
+              <Tab>Todos</Tab>
+              <Tab>Bebidas</Tab>
+              <Tab>Panaderia</Tab>
+              <Tab>Otros</Tab>
+            </TabList>
+          </Tabs>
+
+          <Grid container spacing={2} overflow="auto">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <Grid xs={6} lg={4} xl={3}>
+                <Card>
+                  <div>
+                    <Typography level="title-lg">Americano</Typography>
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                      <Image src={coffeImg} alt="product-image" />
+                    </AspectRatio>
+                  </div>
+                  <CardContent
+                    orientation="horizontal"
+                    sx={{ justifyContent: "space-between" }}
+                  >
+                    <div>
+                      <Typography level="body-xs">Precio total:</Typography>
+                      <Typography fontSize="lg" fontWeight="lg">
+                        $45.00
+                      </Typography>
+                    </div>
+                    <IconButton size="lg" variant="soft">
+                      <AddIcon />
+                    </IconButton>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Stack>
+        <Sheet
+          component={Stack}
+          variant="outlined"
+          width={1000}
+          spacing={2}
+          p={2}
+        >
+          <Input placeholder="Nombre del cliente" />
+          <Stack direction="row" spacing={1}>
+            <Button variant="solid" color="neutral">
+              Llevar
+            </Button>
+            <Button variant="solid" color="neutral">
+              Lugar
+            </Button>
+            <Button variant="solid" color="neutral">
+              Entrega
+            </Button>
+          </Stack>
+
+          <Divider />
+
+          <Grid container>
+            <Grid xs={4}>
+              <Typography>Elementos</Typography>
+            </Grid>
+            <Grid xs={4}>
+              <Typography>Elementos</Typography>
+            </Grid>
+            <Grid xs={4}>
+              <Typography>Precios</Typography>
+            </Grid>
+            <Grid xs={4}>
+              <Typography>Tacos al pastor</Typography>
+              <Typography>$124.00</Typography>
+            </Grid>
+            <Grid xs={4}>
+              <Stack direction="row" alignItems="center" spacing={1}>
+                <IconButton size="sm">
+                  <RemoveIcon />
+                </IconButton>
+                <Typography>2</Typography>
+                <IconButton size="sm">
+                  <AddIcon />
+                </IconButton>
+              </Stack>
+            </Grid>
+            <Grid xs={4}>
+              <Typography>$248.00</Typography>
+            </Grid>
+          </Grid>
+          
+          <Divider sx={{ mt: "auto !important" }} />
+
+          <Textarea placeholder="Agregar nota..." minRows={3} />
+
+          <Stack spacing={1}>
+            <Divider />
+            <Stack direction="row" justifyContent="space-between">
+              <Typography>Subtotal</Typography>
+              <Typography>$248.00</Typography>
             </Stack>
-         </Box>
-      </React.Fragment>
-   );
+            <Stack direction="row" justifyContent="space-between">
+              <Typography>Descuento</Typography>
+              <Typography>$0.00</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography>Impuestos</Typography>
+              <Typography>$0.00</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography>Total</Typography>
+              <Typography>$248.00</Typography>
+            </Stack>
+            <Button variant="solid" color="primary" fullWidth>
+              Pagar
+            </Button>
+          </Stack>
+        </Sheet>
+      </Stack>
+    </DashboardLayout>
+  );
 }
